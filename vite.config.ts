882d96f768906070560785787cf634b7bb0bead7
devyusron <muhammad.yusron@jadinpratama.com>
2025-12-03 14:33:25 +0700

    feat: Menambah konfigurasi base vite untuk gh-pages dan push dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,22 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/", 
   plugins: [
     react(),
     svgr({
       svgrOptions: {
         icon: true,
-        // This will transform your SVG to a React component
         exportType: "named",
         namedExport: "ReactComponent",
       },
     }),
   ],
+  build: {
+    outDir: 'dist',
+  }
 });
